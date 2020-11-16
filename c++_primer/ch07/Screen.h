35987fa6cc3e2034a32145452cab567cdd36a94c
Aananth C N <c.n.aananth@gmail.com>
2020-11-16 21:38:39 +0530

    Improved display output style

--- c++_primer/ch07/Screen.h
+++ c++_primer/ch07/Screen.h
@@ -40,20 +40,25 @@ private:
 
 inline
 void Screen::do_display(ostream& os) const {
-    os << "[\n";
+    os << "\n\nDisplay Contents:\n[ ";
     for (auto i = 0; i < contents.length(); i++) {
         if (contents[i] != ' ')
             os << contents[i];
-        else
-            os << '.';
-        if ((i != 0) && ((i+1) % width == 0)) {
-            os << "\n";
+        else {
+            // print dots instead of space for better visualization
+            os << '.'; 
         }
-        //else {
-        //    os << " ";
-        //}
+        if ((i != 0) && ((i+1) % width == 0)) {
+            // check if this is last row
+            if (i > (width * (height > 1 ? height-1 : -1)))
+                os << " ]\n";
+            else {
+                // not a last line
+                os << "\n  ";
+            }
+        }
     }
-    os << "] size: " << width << " x " << height << endl;
+    os << "size: " << width << " x " << height << endl;
 }
 
 inline
--- c++_primer/ch07/WindowMgr.cpp
+++ c++_primer/ch07/WindowMgr.cpp
@@ -16,4 +16,7 @@ int main() {
     Screen& scr = wmgr.get_screen(0);
     scr.set(1, 1, 'A').set(2,2, 'B');
     wmgr.display_screen(0, cout);
+    wmgr.clear(0);
+    scr.set(12, 36, 'A').set(12, 37, 'a').set(12,38,'n').set(12,39,'a').set(12,40,'n').set(12,41,'t').set(12,42,'h');
+    wmgr.display_screen(0, cout);
 }
